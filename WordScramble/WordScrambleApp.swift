377eb94b6242e7b8e03f814fcc3deeeed1fcd100
stank0v <hr.stank0v@gmail.com>
2024-05-06 15:27:20 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Hristo Stankov on 4.05.24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
